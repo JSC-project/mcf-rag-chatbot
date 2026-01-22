98605e83aa2e25d2bb65a14018e8a76b6243166e
Casper Zanichell <casperzanichelli98@gmail.com>
2026-01-22 15:14:25 +0100

    Merge main in i lancedb-setup och fixar konflikt i data_models

--- src/mcf_rag_chatbot/backend/data_models.py
+++ src/mcf_rag_chatbot/backend/data_models.py
@@ -1,4 +1,22 @@
 from pydantic import BaseModel, Field
+from lancedb.embeddings import get_registry
+from lancedb.pydantic import LanceModel, Vector
+from dotenv import load_dotenv
+import os
+
+load_dotenv() #Read the .env file for API-KEY
+
+embeddings_model = get_registry().get("gemini-text").create(name="models/text-embedding-004") #Initialize the Gemini embedding model registry
+
+class MCFContent(LanceModel):
+    # Data model for storing scraped MCF content in LanceDB.
+
+    url: str = Field(description="Käll-URL för den skrapade sidan för citat")
+    title: str = Field(description="Titel på sidan eller specifikt avsnitt")
+
+    # The SourceField indicates which text should be sent to the embedding model
+    content: str = embeddings_model.SourceField()
+from pydantic import BaseModel, Field
 
 class RagResponse(BaseModel):
     answer: str = Field(description="Short answer based on retrieved content.")
