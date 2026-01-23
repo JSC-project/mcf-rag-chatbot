19a8fd9d0da4175b7f39810eb893481c3ba86b55
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2026-01-23 14:29:55 +0100

    quick api setup and test app for frontend

--- src/mcf_rag_chatbot/backend/data_models.py
+++ src/mcf_rag_chatbot/backend/data_models.py
@@ -36,3 +36,6 @@ class MCFContent(LanceModel):
 class RagResponse(BaseModel):
     answer: str = Field(description="Short answer based on retrieved content.")
     source_url: str | None = Field(default=None, description="URL to the source used.")
+    
+class Prompt(BaseModel):
+    prompt: str
