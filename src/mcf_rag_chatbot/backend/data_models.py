6b44326275ee072ceeed6f20f61371562e2c9d62
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2026-01-26 11:44:03 +0100

    Added an import in data_models

--- src/mcf_rag_chatbot/backend/data_models.py
+++ src/mcf_rag_chatbot/backend/data_models.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime
 from typing import Optional
+import lancedb.embeddings.google
 
 load_dotenv() #Read the .env file for API-KEY
 
