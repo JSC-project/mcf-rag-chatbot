000cb7b2efdceff585ade27179e0a00fb0d96451
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2026-01-26 10:42:22 +0100

    some cleanup. Had two of some functions in data_models

--- src/mcf_rag_chatbot/backend/api.py
+++ src/mcf_rag_chatbot/backend/api.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # root page message
 @app.get("/")
 def root():
-    return {"Restaurant API is runnin. http://127.0.0.1:8000/docs for swagger UI"}
+    return {"Chatbot API is running. http://127.0.0.1:8000/docs for swagger UI"}
 
 # api query function
 @app.post("/rag/query")
