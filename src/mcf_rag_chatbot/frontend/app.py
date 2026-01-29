f2f149114319b5dcf7a628a125a679f2befc110d
Casper Zanichell <casperzanichelli98@gmail.com>
2026-01-29 14:48:53 +0100

    some redesign in streamlit, more basic but working style

--- src/mcf_rag_chatbot/frontend/app.py
+++ src/mcf_rag_chatbot/frontend/app.py
@@ -48,8 +48,10 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+prompt = st.chat_input("Ställ din fråga...")
+
 # User input and RAG Integration
-if prompt := st.chat_input("Ställ din fråga..."):
+if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
